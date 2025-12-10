f8a1027413a496fc9b13c45339d43312733fbda3
SanjaiPS-tech <sanjaips.tech@gmail.com>
2025-12-10 20:06:14 +0530

    final c to git

--- src/services/gemini.js
+++ src/services/gemini.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const API_KEY = "AIzaSyBfNbXGf8pptUS_SRl0dy-blDKDE0zd_XQ";
+const API_KEY = "your_api_key";
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
